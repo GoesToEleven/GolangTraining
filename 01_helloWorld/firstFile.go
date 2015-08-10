22ddef81e673530a6e5990ee412dc5a43a17b149
GoesToEleven <tuddleymc@gmail.com>
2015-08-09 21:54:32 -0700

    adds helloWorld

---
+++ 01_helloWorld/firstFile.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world!")
+}
+
+/*
+
+clarification:
+
+my workspace is called only "go"
+so in my GOPATH you will see it pointing to:
+
+GOPATH="/Users/tm002/Documents/go"
+
+*/
