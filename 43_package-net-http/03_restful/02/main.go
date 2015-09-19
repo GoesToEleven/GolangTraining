814238aa581f91d336fc946f1fc71f683db7807d
GoesToEleven <tuddleymc@gmail.com>
2015-09-19 00:57:36 -0700

    adds to restful example

---
+++ 43_package-net-http/03_restful/02/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+type myHandler int
+
+func (h myHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	switch req.URL.Path {
+	case "/cat":
+		io.WriteString(res, "<strong>kitty kitty kitty<strong>")
+	case "/dog":
+		io.WriteString(res, "<strong>doggy doggy doggy<strong>")
+	}
+}
+
+
+func main() {
+
+	var h myHandler
+	http.ListenAndServe(":9000", h)
+}
