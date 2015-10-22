546cc2ebe8ac3e2dbeb243c61fb74bdd0ce6d5b1
GoesToEleven <tuddleymc@gmail.com>
2015-10-21 23:25:32 -0700

    cryptography aes

---
+++ 99_svcc/39_AES-encrypt-decrypt/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+)
+
+func main() {
+	// The key argument should be the AES key,
+	// either 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256.
+	key := "opensesame123456" // 16 bytes!
+	block, _ := aes.NewCipher([]byte(key))
+	fmt.Printf("%d bytes NewCipher key with block size of %d bytes\n", len(key), block.BlockSize)
+	str := []byte("Hello World!")
+	// 16 bytes for AES-128, 24 bytes for AES-192, 32 bytes for AES-256
+	ciphertext := []byte("abcdef1234567890")
+	iv := ciphertext[:aes.BlockSize] // const BlockSize = 16
+	// encrypt
+	encrypter := cipher.NewCFBEncrypter(block, iv)
+	encrypted := make([]byte, len(str))
+	encrypter.XORKeyStream(encrypted, str)
+	fmt.Printf("%s encrypted to %v\n", str, encrypted)
+	// decrypt
+	decrypter := cipher.NewCFBDecrypter(block, iv) // simple!
+	decrypted := make([]byte, len(str))
+	decrypter.XORKeyStream(decrypted, encrypted)
+	fmt.Printf("%v decrypt to %s\n", encrypted, decrypted)
+}
+
+// https://www.socketloop.com/tutorials/golang-how-to-encrypt-with-aes-crypto
