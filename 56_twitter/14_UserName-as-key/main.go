d3e7206cf48d87c9505140bb8f035f9c6eac8ae0
GoesToEleven <tuddleymc@gmail.com>
2015-10-22 11:38:29 -0700

    code review and improvement

---
+++ 56_twitter/14_UserName-as-key/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"html/template"
+	"net/http"
+	"google.golang.org/appengine/log"
+	stdlog "log"
+	"io/ioutil"
+)
+
+type User struct {
+	Email    string
+	UserName string
+	Password string
+}
+
+var tpl *template.Template
+
+func init() {
+	r := httprouter.New()
+	http.Handle("/", r)
+	r.GET("/", Home)
+	r.GET("/form/login", Login)
+	r.GET("/form/signup", Signup)
+	r.POST("/api/checkusername", checkUserName)
+	r.POST("/api/createuser", createUser)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("public/"))))
+	tpl = template.Must(template.ParseGlob("templates/html/*.html"))
+}
+
+func Home(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+	tpl.ExecuteTemplate(res, "home.html", nil)
+}
+
+func Login(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	tpl.ExecuteTemplate(res, "login.html", nil)
+}
+
+func Signup(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	tpl.ExecuteTemplate(res, "signup.html", nil)
+}
+
+func checkUserName(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	ctx := appengine.NewContext(req)
+	bs, err := ioutil.ReadAll(req.Body)
+	sbs := string(bs)
+	stdlog.Println("REQUEST BODY: ", sbs)
+	q, err := datastore.NewQuery("Users").Filter("UserName=", sbs).Count(ctx)
+	stdlog.Println("ERR: ", err)
+	stdlog.Println("QUANTITY: ", q)
+	if err != nil {
+		fmt.Fprint(res, "false")
+		return
+	}
+	if q >= 1 {
+		fmt.Fprint(res, "true")
+	} else {
+		fmt.Fprint(res, "false")
+	}
+}
+
+func createUser(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	ctx := appengine.NewContext(req)
+	NewUser := User{
+		Email:    req.FormValue("email"),
+		UserName: req.FormValue("userName"),
+		Password: req.FormValue("password"),
+	}
+	key := datastore.NewIncompleteKey(ctx, "Users", nil)
+	key, err := datastore.Put(ctx, key, &NewUser)
+	// this is the only error checking I added; any others on this page needed?
+	if err != nil {
+		log.Errorf(ctx, "error adding todo: %v", err)
+		http.Error(res, err.Error(), 500)
+		return
+	}
+	http.Redirect(res, req, "/", 302)
+}
