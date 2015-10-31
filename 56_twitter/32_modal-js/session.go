6b7004e6e8430edfd7e284d7fac4f917b58cadf7
GoesToEleven <tuddleymc@gmail.com>
2015-10-31 14:19:16 -0700

    modal js

---
+++ 56_twitter/32_modal-js/session.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/memcache"
+	"net/http"
+)
+
+func getSession(req *http.Request) *memcache.Item {
+	cookie, err := req.Cookie("session")
+	if err != nil {
+		return &memcache.Item{}
+	}
+
+	ctx := appengine.NewContext(req)
+	item, err := memcache.Get(ctx, cookie.Value)
+	if err != nil {
+		return &memcache.Item{}
+	}
+	return item
+}
