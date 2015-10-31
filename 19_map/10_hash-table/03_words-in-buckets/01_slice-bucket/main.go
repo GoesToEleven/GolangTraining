82cf041aec7d32c4655a2312329e6e76b2819509
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2015-10-30 22:01:54 -0700

    coolness

---
+++ 19_map/10_hash-table/03_words-in-buckets/01_slice-bucket/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// get the book adventures of sherlock holmes
+	res, err := http.Get("http://www.gutenberg.org/cache/epub/1661/pg1661.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// scan the page
+	scanner := bufio.NewScanner(res.Body)
+	defer res.Body.Close()
+	// Set the split function for the scanning operation.
+	scanner.Split(bufio.ScanWords)
+	// Create slice of slice of string to hold slices of words
+	buckets := make([][]string, 12)
+	// Create slices to hold words words
+	for i := 0; i < 12; i++ {
+		buckets = append(buckets, []string{})
+	}
+	// Loop over the words
+	for scanner.Scan() {
+		word := scanner.Text()
+		n := HashBucket(word, 12)
+		buckets[n] = append(buckets[n], word)
+	}
+	// Print len of each bucket
+	for i := 0; i < 12; i++ {
+		fmt.Println(i, " - ", len(buckets[i]))
+	}
+	// Print the words in one of the buckets
+	// fmt.Println(buckets[6])
+}
+
+func HashBucket(word string, buckets int) int {
+	var sum int
+	for _, v := range word {
+		sum += int(v)
+	}
+	return sum % buckets
+}
---
+++ 19_map/10_hash-table/03_words-in-buckets/02_map-bucket/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// get the book adventures of sherlock holmes
+	res, err := http.Get("http://www.gutenberg.org/cache/epub/1661/pg1661.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// scan the page
+	scanner := bufio.NewScanner(res.Body)
+	defer res.Body.Close()
+	// Set the split function for the scanning operation.
+	scanner.Split(bufio.ScanWords)
+	// Create map with a key of int
+	// and a value of another map
+	// with a key of string, which will be the word
+	// and a value of int, which will be the number of times the word occurs
+	buckets := make(map[int]map[string]int)
+	// Create slices to hold words words
+	for i := 0; i < 12; i++ {
+		buckets[i] = make(map[string]int)
+	}
+	// Loop over the words
+	for scanner.Scan() {
+		word := scanner.Text()
+		n := HashBucket(word, 12)
+		buckets[n][word]++
+	}
+	// Print words in a bucket
+	for k, v := range buckets[6] {
+		fmt.Println(v, " \t- ", k)
+	}
+}
+
+func HashBucket(word string, buckets int) int {
+	var sum int
+	for _, v := range word {
+		sum += int(v)
+	}
+	return sum % buckets
+}
