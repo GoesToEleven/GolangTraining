4a339e01b9c38ea3d2476406467e366c7ccffb09
GoesToEleven <tuddleymc@gmail.com>
2015-11-04 01:15:46 -0800

    soiree

--- 19_map/10_hash-table/03_words-in-buckets/01_slice-bucket/main.go
+++ 19_map/10_hash-table/03_words-in-buckets/01_slice-bucket/main.go
@@ -36,7 +36,7 @@ func main() {
 		fmt.Println(i, " - ", len(buckets[i]))
 	}
 	// Print the words in one of the buckets
-	// fmt.Println(buckets[6])
+	 fmt.Println(buckets[6])
 }
 
 func HashBucket(word string, buckets int) int {
---
+++ 56_twitter/35_schmidt-params/05/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"log"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	if len(ps) != 0 {
+		fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+		return
+	}
+
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/user/*name", Index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
