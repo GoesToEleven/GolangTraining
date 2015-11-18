60d8eb431ae2c700bdc0a9c4512d61b90fb16ad7
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2015-11-18 12:31:30 -0800

    slices

--- 18_slice/12_multi-dimensional/04_comparing_shorthand_var_make/01_shorthand-slice/main.go
+++ 18_slice/12_multi-dimensional/04_comparing_shorthand_var_make/01_shorthand-slice/main.go
@@ -7,8 +7,8 @@ import (
 func main() {
 	student := []string{}
 	students := [][]string{}
-	// student[0] = "Todd"
-	student = append(student, "Todd")
+	student[0] = "Todd"
+	// student = append(student, "Todd")
 	fmt.Println(student)
 	fmt.Println(students)
 }
