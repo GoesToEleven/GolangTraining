e685841ee6357e23b2b2e7ef66be618f3e3bf4cb
GoesToEleven <tuddleymc@gmail.com>
2015-11-19 13:17:56 -0800

    adds jwt.Config to init

--- 59_appengine-GCS-storage/02_NewWriter_JSON-auth/storage.go
+++ 59_appengine-GCS-storage/02_NewWriter_JSON-auth/storage.go
@@ -11,29 +11,32 @@ import (
 	"google.golang.org/cloud"
 	"google.golang.org/cloud/storage"
 	"io/ioutil"
+	"golang.org/x/oauth2/jwt"
 )
 
 const gcsBucket = "learning-1130-bucket-01"
 const aeId = "learning-1130"
+var conf *jwt.Config
 
 func init() {
 	http.HandleFunc("/put", handlePut)
-}
 
-func getCloudContext(req *http.Request) (context.Context, error) {
 	jsonKey, err := ioutil.ReadFile("gcs.xxjson")
 	if err != nil {
-		return nil, err
+		panic("Couldn't read json key")
 	}
 
-	conf, err := google.JWTConfigFromJSON(
+	conf, err = google.JWTConfigFromJSON(
 		jsonKey,
 		storage.ScopeFullControl,
 	)
 	if err != nil {
-		return nil, err
+		panic("Couldn't get *jwt.Config")
 	}
 
+}
+
+func getCloudContext(req *http.Request) (context.Context, error) {
 	ctx := appengine.NewContext(req)
 	hc := conf.Client(ctx)
 	return cloud.NewContext(aeId, hc), nil
@@ -47,9 +50,9 @@ func handlePut(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	writer := storage.NewWriter(cctx, gcsBucket, "exampleYALLSTILL.txt")
+	writer := storage.NewWriter(cctx, gcsBucket, "myOffice.txt")
 	writer.ContentType = "text/plain"
-	io.WriteString(writer, "in class with y'all STILL")
+	io.WriteString(writer, "in my office")
 	err = writer.Close()
 	if err != nil {
 		http.Error(res, "ERROR WRITING TO BUCKET: "+err.Error(), 500)
