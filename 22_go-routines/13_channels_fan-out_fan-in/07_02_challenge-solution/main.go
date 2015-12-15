c6b7d0095e9437118fdeed67a30e1a998fdebf51
GoesToEleven <tuddleymc@gmail.com>
2015-12-15 11:01:06 -1000

    aloha

--- 22_go-routines/13_channels_fan-out_fan-in/07_02_challenge-solution/main.go
+++ 22_go-routines/13_channels_fan-out_fan-in/07_02_challenge-solution/main.go
@@ -9,21 +9,29 @@ func main() {
 
 	in := gen()
 
-	f := fanOut(in)
+	// Distribute the work across ten goroutines that all read from in.
+	c0 := factorial(in)
+	c1 := factorial(in)
+	c2 := factorial(in)
+	c3 := factorial(in)
+	c4 := factorial(in)
+	c5 := factorial(in)
+	c6 := factorial(in)
+	c7 := factorial(in)
+	c8 := factorial(in)
+	c9 := factorial(in)
 
-	for n := range f {
+	// Consume the merged output from c0 through c9
+	for n := range merge(c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) {
 		fmt.Println(n)
-		fmt.Printf("%T", n)
 	}
-
-	fmt.Printf("%T\n", f)
 
 }
 
 func gen() <-chan int {
 	out := make(chan int)
 	go func() {
-		for i := 0; i < 100; i++ {
+		for i := 0; i < 10; i++ {
 			for j := 3; j < 13; j++ {
 				out <- j
 			}
@@ -33,28 +41,48 @@ func gen() <-chan int {
 	return out
 }
 
-func fanOut(in <-chan int) <-chan <-chan int {
-
-	wg := sync.WaitGroup{}
-	out := make(chan (<-chan int))
-	defer close(out)
-
-	for n := range in {
-		go func() {
-			out <- factorial(n)
-		}()
-	}
-
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
 	return out
 }
 
-func factorial(n int) <-chan int {
-	c := make(chan int)
+func fact(n int) int {
 	total := 1
 	for i := n; i > 0; i-- {
 		total *= i
 	}
-	c <- total
-	close(c)
-	return c
+	return total
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	// Start an output goroutine for each input channel in cs.
+	// output copies values from c to out until c is closed, then calls wg.Done.
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
 }
