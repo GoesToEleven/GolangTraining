fea6fc595fe53f158a2ca6a7d630770dee1ff639
GoesToEleven <tuddleymc@gmail.com>
2016-03-10 12:13:07 -0800

    changes 16 exercise solution 3 to account for only negative numbers

--- 16_exercise-solutions/03_variadic-greatest/main.go
+++ 16_exercise-solutions/03_variadic-greatest/main.go
@@ -16,3 +16,32 @@ func main() {
 	greatest := max(4, 7, 9, 123, 543, 23, 435, 53, 125)
 	fmt.Println(greatest)
 }
+
+/*
+FYI
+For your code to also work with only negative numbers such as
+
+greatest := max(-200 -700)
+
+include this as your range statement
+for i, v := range numbers {
+	if v > largest || i == 0 {
+		largest = v
+	}
+}
+
+What does that code do?
+
+The first time through the range loop
+the index, i, will be zero
+so largest will be set to the first number
+
+Originally largest is set to the zero value for an int, which is zero
+
+Zero would be greater than any negative number
+
+if you only have negative numbers
+you need largest to be something less than zero
+
+Thanks to Ricardo G for this code improvement!
+*/
