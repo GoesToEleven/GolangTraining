9517336314b9a5b2fe1608a8d7f227456382b90f
GoesToEleven <tuddleymc@gmail.com>
2016-03-25 07:26:24 -0700

    adds note to file

--- 10_for-loop/07_rune-loop_UTF8/01/main.go
+++ 10_for-loop/07_rune-loop_UTF8/01/main.go
@@ -3,10 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	for i := 50; i <= 140; i++ {
+	for i := 250; i <= 340; i++ {
 		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
 	}
 	foo := "a"
 	fmt.Println(foo)
 	fmt.Printf("%T \n", foo)
 }
+
+/*
+NOTE:
+Some operating systems (Windows) might not print characters where i < 256
+
+If you have this issue, you can use this code:
+
+fmt.Println(i, " - ", string(i), " - ", []int32(string(i)))
+
+UTF-8 is the text coding scheme used by Go.
+
+UTF-8 works with 1 - 4 bytes.
+
+A byte is 8 bits.
+
+[]byte deals with bytes, that is, only 1 byte (8 bits) at a time.
+
+[]int32 allows us to store the value of 4 bytes, that is, 4 bytes * 8 bits per byte = 32 bits.
+*/
