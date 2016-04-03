a1400e815ad644d0ec6a8efd404421eed047fc48
GoesToEleven <tuddleymc@gmail.com>
2016-04-03 15:07:43 -0700

    concurrency goroutine channel example

---
+++ 25_QUESTIONS-FROM-STUDENTS/05_concurrency-channels/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 10
+	c := make(chan int)
+	done := make(chan bool)
+
+	go func() {
+		for i := 0; i < 1000; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			for q := range c {
+				fmt.Println("i", i, "q", q)
+			}
+			done <- true
+		}(i)
+	}
+
+	for i := 0; i < n; i++ {
+		<-done
+	}
+}
