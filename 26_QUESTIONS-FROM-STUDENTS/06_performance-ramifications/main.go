a342bfc46f74bd4cf5f716e492cfac9fccbb8019
GoesToEleven <tuddleymc@gmail.com>
2016-04-28 22:20:30 -0700

    performance ramifications

---
+++ 26_QUESTIONS-FROM-STUDENTS/06_performance-ramifications/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	in := gen()
+
+	f := factorial(in)
+
+	for n := range f {
+		fmt.Println(n)
+	}
+}
+
+func gen() <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			for j := 3; j < 13; j++ {
+				out <- j
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- fact(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fact(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
