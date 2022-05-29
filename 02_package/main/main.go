534eac92a06618e194a98685a6669367873d4ba6
Bello mahmud <75342173+mahmudsudo@users.noreply.github.com>
2022-05-29 21:47:01 +0100

    updated imports in main.go
    added local imports

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -5,6 +5,12 @@ import (
 	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
 	"github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	/*for local imports 
+	this works only after using ``` go mod init 02_package ``` in the 02_package directory
+	//02_package/stringutil
+	//02_package/icomefromalaska
+	// the local imports works as fine as the github imports and are even shorter and easier to understand
+	
 )
 
 func main() {
